f37b69899765611688c8b2106219ea38eb4ad22d
lauretteroy <laurieroy.dev@gmail.com>
2022-02-05 18:43:33 -0800

    Update cors, add letter-opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,4 +66,11 @@ Rails.application.configure do
 
   # devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # letter opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
 end
